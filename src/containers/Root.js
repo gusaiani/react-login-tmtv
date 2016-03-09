3393777ae98c51fe0568ec191c49883796057490
Gustavo Saiani <gustavo@poe.ma>
2016-03-09 14:25:38 -0300

    Reformat Root component

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import routes from '../routes';
-import {ReduxRouter} from 'redux-router';
+import React from 'react'
+import {Provider} from 'react-redux'
+import routes from '../routes'
+import {ReduxRouter} from 'redux-router'
 
 export default class Root extends React.Component {
+  static propTypes = {
+    store: React.PropTypes.object.isRequired
+  }
 
-    static propTypes = {
-        store: React.PropTypes.object.isRequired
-    };
-
-    render () {
-        return (
-            <div>
-                <Provider store={this.props.store}>
-                    <div>
-                        <ReduxRouter>
-                            {routes}
-                        </ReduxRouter>
-                    </div>
-                </Provider>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <Provider store={this.props.store}>
+          <div>
+            <ReduxRouter>
+              {routes}
+            </ReduxRouter>
+          </div>
+        </Provider>
+      </div>
+    )
+  }
 }
